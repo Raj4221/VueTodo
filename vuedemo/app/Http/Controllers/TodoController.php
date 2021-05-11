fea611d3f1d775a4fa56bdb655a9398b245773d9
Raj4221 <80743286+Raj4221@users.noreply.github.com>
2021-05-11 10:06:49 +0530

    Add files via upload

---
+++ vuedemo/app/Http/Controllers/TodoController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        // $student=Todo::paginate(5);
+        // return view("welcome")->with('student',$student);
+        return Todo::latest()->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        // $student=new todo();
+        // $student->name=$request->name;
+        // $student->address=$request->address;
+        // $student->contact=$request->contact;
+        // $student->save();
+        // return back();
+        Todo::create($request->all());   
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, todo $todo)
+    {
+        //
+        $todo->update($request->all());
+        $todo->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+        return Todo::latest()->get();
+    }
+}
